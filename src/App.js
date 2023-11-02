b79f1cb51e677a2e752dfafb15f0c68b31c7a963
fahad0007 <fa4983312@gmail.com>
2023-11-02 17:26:23 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-// import DeleteUserApi from './Components/DeleteUserApi';
-// import Dropdown from './Components/Dropdown';
-import PhotosApi from './Components/PhotosApi/PhotosApi';
-import Search from './Components/Search';
+import DeleteUserApi from './Components/DeleteUserApi';
 
 
-// import Search from './Components/Search';
+
+
 
 
 
@@ -13,11 +11,9 @@ import Search from './Components/Search';
 function App() {
   return (
     <div className="App">
-      {/* <Dropdown/> */}
+     
       <DeleteUserApi/>
-      {/* <Search/> */}
-      {/* <PhotosApi/> */}
-    
+      
     </div>
   );
 }
